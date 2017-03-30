b0b2012bc4d1764383b3e69fdabe2114737877ed
Phillip Kolodziejski <phillip.kolodziejski@gmail.com>
2017-03-30 22:09:48 +1100

    bindings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,21 +16,36 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('lifeInsuredDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+// .controller('lifeInsuredDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// // You can include any angular dependencies as parameters for this function
+// // TIP: Access Route Parameters for your page via $stateParams.parameterName
+// function ($scope, $stateParams) {
 
-
-}])
+// }])
    
-.controller('lifeInsuredDetails2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+.controller('lifeInsuredDetailsCtrl', function ($scope, LifeInsured) {
+  LifeInsured.get().success(function (response) {
+    $scope.lifeInsured = response;
+    //Phil.K - Debug
+    console.log($scope.lifeInsured);
+  })
+})
+
+// .controller('lifeInsuredDetails2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// // You can include any angular dependencies as parameters for this function
+// // TIP: Access Route Parameters for your page via $stateParams.parameterName
+// function ($scope, $stateParams) {
 
 
-}])
+// }])
+
+.controller('lifeInsuredDetails2Ctrl', function ($scope, LifeInsured) {
+  LifeInsured.get().success(function (response) {
+    $scope.lifeInsured = response;
+    //Phil.K - Debug
+    console.log($scope.lifeInsured);
+  })
+})
    
 .controller('policyOwnerDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
--- www/js/services.js
+++ www/js/services.js
@@ -4,6 +4,27 @@ angular.module('app.services', [])
 
 }])
 
+.factory('LifeInsuredFactory', [function(){
+
+}])
+
+.factory('LifeInsured', function ($http, $rootScope, $stateParams) {
+
+  return {
+    all: function () {
+      return $http.get('https://friends.json/all', { params: { user_id: $rootScope.session } })
+    },
+    get: function () {
+      return $http.get('http://ec2-13-54-111-58.ap-southeast-2.compute.amazonaws.com/api/v1/getlifeinsured?id=1', { params: { firstname: $rootScope.firstname, 
+          surname: $stateParams.surname, email: $stateParams.email, dob: $stateParams.dob, addressline1: $stateParams.addressline1, addressline2: $stateParams.addressline2,
+         suburb: $stateParams.suburb, state: $stateParams.state, postcode: $stateParams.postcode, country: $stateParams.country} })
+    },
+    add: function (id) {
+      return $http.get('https://friends.json/new', { params: {idfriends:id}})
+    }
+  };
+})
+
 .service('BlankService', [function(){
 
 }]);
